16277b291a93685aa19b542610a16846effcf008
Tom Hanley <thanley11@gmail.com>
2016-06-17 21:01:22 -0400

    Fixed for SuaveIO w/ Angular 2, adding Angular 2 starter app

--- Program.fs
+++ Program.fs
@@ -15,18 +15,45 @@ open System.Threading
 type CmdArgs = { IP: System.Net.IPAddress; Port: Sockets.Port }
 
 let defaultArgs = { IP = System.Net.IPAddress.Loopback; Port = 8083us }
+let logger = Logging.Loggers.ConsoleWindowLogger Logging.LogLevel.Verbose
+
+let refreshEvent = new Event<WebSocket>()
+
+let socketHandler (webSocket : WebSocket) =
+  fun cx -> socket {
+    while true do
+      let! refreshed =
+        Control.Async.AwaitEvent(refreshEvent.Publish)
+        |> Suave.Sockets.SocketOp.ofAsync
+      do! webSocket.send Text (ASCII.bytes "refreshed") true
+  }
+
 let webConfig = 
-    { 
-        defaultConfig with 
-            bindings=[ HttpBinding.mk HTTP defaultArgs.IP defaultArgs.Port ] 
-            logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Verbose
+    { defaultConfig with 
+        homeFolder = Some (__SOURCE_DIRECTORY__)
+        bindings=[ HttpBinding.mk HTTP defaultArgs.IP defaultArgs.Port ] 
+        logger = logger
+        listenTimeout = TimeSpan.FromMilliseconds 3000.
     }
+
+let app : WebPart =
+  choose [
+    Filters.log logger logFormat >=> never
+    Filters.path "/websocket" >=> handShake socketHandler
+    Filters.GET >=> Filters.path "/" >=> file "index.html"
+    Writers.setHeader "Cache-Control" "no-cache, no-store, must-revalidate"
+      >=> Writers.setHeader "Pragma" "no-cache"
+      >=> Writers.setHeader "Expires" "0"
+      >=> browseHome
+    NOT_FOUND "Found no handlers."
+  ]
 
 [<EntryPoint>]
 let main argv = 
     
     let cts = new CancellationTokenSource()
-    let startingServer, shutdownServer = startWebServerAsync webConfig (Successful.OK "Hello World!")
+    (*let startingServer, shutdownServer = startWebServerAsync webConfig (Successful.OK "Hello World!")*)
+    let startingServer, shutdownServer = startWebServerAsync webConfig app
 
     Async.Start(shutdownServer, cts.Token)
 
